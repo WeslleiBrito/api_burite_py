849b5f47ac4dadb9fcde9971ce095d64b7b55204
weslleiBrito <wesllei.wbs@gmail.com>
2025-06-06 17:22:07 -0300

    correção do database para limitar as requisições

--- app/db/connection.py
+++ app/db/connection.py
@@ -2,4 +2,11 @@ from sqlalchemy import create_engine
 
 DATABASE_URL = "mysql+mysqlconnector://burite:burite123@192.168.0.112:3307/clarionerp"
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=10,
+    max_overflow=20,
+    pool_timeout=30,
+    pool_recycle=1800
+)
