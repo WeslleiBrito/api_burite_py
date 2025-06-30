62d6e86c1832fd12881c51f49073727841c5e0ff
weslleiBrito <wesllei.wbs@gmail.com>
2025-06-30 11:14:08 -0300

    correção do enderço url do resumo financeiro

--- app/api/routes/resume_financeiro_route.py
+++ app/api/routes/resume_financeiro_route.py
@@ -8,7 +8,7 @@ router_resumo = APIRouter()
 
 resumo_financeiro = ResumoFinanceiroService()
 
-@router_resumo.get("/")
+@router_resumo.get("")
 def resumo(
     data_inicial: Optional[date] = Query(None, title="Data Inicial", description="Data de início do período (yyyy-mm-dd)"),
     data_final: Optional[date] = Query(None, title="Data Final", description="Data de fim do período (yyyy-mm-dd)")
